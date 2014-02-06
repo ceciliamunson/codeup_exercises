4bc90595a7f2565f81d3601b23843cd18f457914
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-06 14:42:16 -0600

    Added else exercise

--- php/if.php
+++ php/if.php
@@ -1,7 +1,7 @@
 <?php
 $a = 5;
 $b = 10;
-$c = '10';
+$c = '20';
 
 if ($a < $b) {
 	echo "$a is less than $b\n";
@@ -28,6 +28,33 @@ if ($b != $c) {
 if ($b !== $c) {
 	echo "$b is not identical to $c\n";
 
+}
+if ($a < $b) {
+	echo "$a is less than $b\n";
+}
+else {
+	echo "$a is greater than $b\n";
+}
+if ($b >= $c) {
+	echo "$b is greater than or equal to $c\n";
+}
+else {
+	echo "$b is less than or equal to $c\n";
+}
+
+// Else exercise:
+
+if ($b === $c) {
+	echo "$b is identical to $c\n";
+}
+elseif ($b == $c) {
+	echo "$b is equal to $c\n";
+}
+elseif ($b !== $c) {
+	echo "$b is not identical to $c\n";
+}
+else {
+	echo "$b is not equal to $c\n";
 }
 
 ?>
