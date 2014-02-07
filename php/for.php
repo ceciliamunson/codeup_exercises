a91f63678a8d210e47656025253d5f636c443fc4
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-07 16:44:27 -0600

    User to choose increment

--- php/for.php
+++ php/for.php
@@ -6,9 +6,12 @@ $starting_number = intval(fgets(STDIN));
 //prompt user for ending number
 fwrite(STDOUT, "Enter ending number: ");
 $ending_number = intval(fgets(STDIN));
+//promt user to enter increment
+fwrite(STDOUT, "Enter increment: ");
+$increment = intval(fgets(STDIN));
 
-//display all nubers from starting to ending using for loop
-for ($i = $starting_number; $i <= $ending_number; $i++) {
+//display numbers from starting to ending in specified increment 
+for ($i = $starting_number; $i <= $ending_number; $i += $increment) {
 	echo "{$i}\n";
 }
 
