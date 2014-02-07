4c430344475be5dcbf28228a9702fbcc04db837b
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-07 11:31:48 -0600

    Added command to display number of guesses

--- php/highlow.php
+++ php/highlow.php
@@ -1,9 +1,10 @@
 <?php
-
+$number_guesses = 0;
 $random_number = mt_rand(1, 100);
 do {
 	fwrite(STDOUT, 'Guess number between 1 and 100 ');
 	$user_number = fgets(STDIN);
+	$number_guesses ++;
 	if ($user_number < $random_number) {
 		echo "Higher\n";
 	}
@@ -12,6 +13,7 @@ do {
 	}
 	else {
 		echo "Good guess. You won!\n";
+		echo "Number of guesses: $number_guesses\n";
 	}
 }
 while ($random_number != $user_number);
