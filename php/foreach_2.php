15b527453a9f3b537e17eda7204245063eb2b7c5
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-10 13:21:52 -0600

    Output book's title and then each key and its value

---
+++ php/foreach_2.php
@@ -0,0 +1,36 @@
+<?php
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+
+//output book's title and then each key and its value
+foreach ($books as $key => $book) {
+	echo "$key\n";
+	foreach ($book as $key => $details) {
+		echo "$key: $details\n";
+	}
+	echo "\n";
+
+}
+
+
+?>
