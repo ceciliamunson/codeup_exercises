172e3211005b5101e5c6319d1ae18c3bbb637baf
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-10 14:34:14 -0600

    for loop that shows all even numbers between 1 and 100 using continue

---
+++ php/break_continue.php
@@ -0,0 +1,14 @@
+<?php
+
+//for loop that shows all even numbers between 1 and 100 using continue.
+
+for ($i =1; $i <= 100; $i++) {
+	if ($i % 2 == 0){
+		echo "$i\n";
+		continue;
+	}
+}
+
+
+
+?>
