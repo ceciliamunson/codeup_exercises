5fd5524e5fcbc042a6c6d9c96adac0f975e1ab34
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-10 10:52:56 -0600

    changed starting number to 1

--- php/fizzbuzz.php
+++ php/fizzbuzz.php
@@ -1,18 +1,24 @@
 <?php
-//print numbers from 0 to 100
-for ($number = 0; $number <= 100; $number ++)
-//for multiples of 3 print Fizz instead of number
-//for multiples of 5 print Buzz instead of number
+//print numbers from 1 to 100
+for ($number = 1; $number <= 100; $number ++)
+
+
 //for numbers multiples of 3 and 5 print FizzBuzz
  if ($number % 3 == 0 && $number % 5 == 0) {
  	echo "FizzBuzz\n";
+
+ //for multiples of 3 print Fizz instead of number
  } elseif ($number % 3 == 0) {
  	echo "Fizz\n";
+
+ //for multiples of 5 print Buzz instead of number	
  } elseif ($number % 5 == 0) {
  	echo "Buzz\n";
- } else {
+ } 
+ 	else {
  	echo "$number\n";
  }
+ 
 
  
 
