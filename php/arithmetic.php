e566cae4ef991334221e5a47d8251495a3516559
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-11 13:27:12 -0600

    Error messages to display value of arguments

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,38 +1,50 @@
 <?php
 		
 function add($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	echo ($a + $b) . "\n";
-    }
+	if (!is_numeric($a)) {
+		echo "$a is not a numeric value\n";	
+	}
+	elseif (!is_numeric($b)) {
+		echo "$b is not a numeric value\n";	
+	}
     else {
-		echo "Please enter only numeric values\n";
+		echo ($a + $b) . "\n";
 	}	
 }
 add(1, 4);
 
 function subtract($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	echo ($a - $b) . "\n";
+	if (!is_numeric($a)) {
+		echo "$a is not a numeric value\n";	
+	}
+	elseif (!is_numeric($b)) {
+		echo "$b is not a numeric value\n";	
 	}
 	else {
-		echo "Please enter only numeric values\n";
+		echo ($a - $b) . "\n";
 	}	
 }
 	subtract(20, 5);
 
 function multiply($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	echo ($a * $b) . "\n";
+	if (!is_numeric($a)) {
+		echo "$a is not a numeric value\n";	
+	}
+	elseif (!is_numeric($b)) {
+		echo "$b is not a numeric value\n";	
 	}
 	else {
-		echo "Please enter only numeric values\n";
+		echo ($a * $b) . "\n";
 	}	
 }
 	multiply(3, 2);
 
 function divide($a, $b) {
-	if (!is_numeric($a) || !is_numeric($b)) {
-		echo "Please enter only numeric values\n";	
+	if (!is_numeric($a)) {
+		echo "$a is not a numeric value\n";	
+	}
+	elseif (!is_numeric($b)) {
+		echo "$b is not a numeric value\n";	
 	}
 	elseif ($b == 0) {
 		echo "Dividing by zero is not a valid operation\n";
@@ -45,14 +57,17 @@ function divide($a, $b) {
 
 	//add function that finds the modulus of 2 numbers
 function modulus($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		echo ($a % $b) . "\n";
+	if (!is_numeric($a)) {
+		echo "$a is not a numeric value\n";
+	}
+	elseif (!is_numeric($b)) {
+		echo "$b is not a numeric value\n";	
 	}
 	else {
-		echo "Please enter only numeric values\n";
+		echo ($a % $b) . "\n";
 	}	
 }
-	modulus(a, 5);
+	modulus(10, 5);
 	
 
 
