1c38491b5824c8513305ed7edfd3b19a71ad7e80
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-11 17:09:06 -0600

    Use return instead of echo to show results

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -2,56 +2,59 @@
 		
 // error function if arguments not numeric
 function not_numeric($a, $b) {
-	if (!is_numeric($a)) {
-		echo "$a is not a numeric value\n";
-		exit(0);
+	if (is_numeric($a) && is_numeric($b)) {
+		return TRUE;
 	}
-	if (!is_numeric($b)) {
-		echo "$b is not a numeric value\n";
-		exit(0);
+	else {
+		echo "Both values must be numeric\n";	
+		return FALSE;
 	}
-	
-
 }
 
 //error function if divinding by zero
 function not_div_by_zero($a, $b) {
 	if ($b == 0) {
 		echo "Dividing by zero is not a valid operation\n";
-		exit(0);
+		return FALSE;
 	}
+	else return TRUE;
 	
 }
 
 function add($a, $b) {
- 		not_numeric($a, $b);
-		echo ($a + $b) . "\n";
+	if  (not_numeric($a, $b) == TRUE) {
+		return ($a + $b);
+	}
+
 }
 
 function subtract($a, $b) {
-		not_numeric($a, $b);
-		echo ($a - $b) . "\n";
+	if  (not_numeric($a, $b) == TRUE) {
+		return ($a - $b);
+	}
 }
 	
 function multiply($a, $b) {
-		not_numeric($a, $b);
-		echo ($a * $b) . "\n";
+	if  (not_numeric($a, $b) == TRUE) {
+		return ($a * $b);
+	}
 }
 
 function divide($a, $b) {
-		not_numeric($a, $b);
-		not_div_by_zero($a, $b);
-		echo ($a / $b) . "\n";
+	if  (not_numeric($a, $b) == TRUE && not_div_by_zero($a, $b) == TRUE) {
+		return ($a / $b);
+	}
 }
 
 	//add function that finds the modulus of 2 numbers
 function modulus($a, $b) {
-		not_numeric($a, $b);
-		echo ($a % $b) . "\n";
+	if  (not_numeric($a, $b) == TRUE) {
+		return ($a % $b);
+	}
 }
 
-add(5, 4);
-subtract(20, 5);
-multiply(3, 2);
-divide(20, 10);
-modulus(10, 5);	
+echo add(1, 4) . PHP_EOL;
+echo subtract(20, 5) . PHP_EOL;
+echo multiply(3, 2) . PHP_EOL;
+echo divide(20, 0) . PHP_EOL;
+echo modulus(10, 5) . PHP_EOL;	
