a31122ef2203a046ce1cd89dd4e10f9224585fc7
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-11 11:01:59 -0600

    Added code to function that will return real numbers

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,18 +1,26 @@
 <?php
 
 function add($a, $b) {
+	$a = 30;
+	$b = 10;
     echo $a + $b;
 }
 
 function subtract($a, $b) {
+	$a = 20;
+	$b = 5;
     echo $a - $b;
 }
 
-function multiply($a, $b) {
+function multiply($a, $b) 
+	$a = 3;
+	$b = 2;
     echo $a * $b;
 }
 
 function divide($a, $b) {
+	$a = 45;
+	$b = 9;
     echo $a / $b;
 }
 
