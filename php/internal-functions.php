fbbb4289030ca40a3f36e99b8497449d04dda35a
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-13 01:01:10 -0600

    Function to check if set or empty; serialize and unserialize

---
+++ php/internal-functions.php
@@ -0,0 +1,38 @@
+<?php
+
+$nothing = NULL;
+$something = '';
+$array = array(1,2,3);
+
+// Create a funciton that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+function set_or_empty($var) {
+	if (isset($var)) {
+		return TRUE; 
+	}
+	elseif (empty($var)) {
+		return FALSE;
+	}
+		
+}
+// TEST: If var $nothing is set, display '$nothing is SET'
+if (set_or_empty($nothing)) {
+	echo "\$nothing is SET\n";
+}
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+if (set_or_empty($nothing) == FALSE) {
+	echo "\$nothing is EMPTY\n";
+}
+// TEST: If var $something is set, display '$something is SET'
+if (set_or_empty($something)) {
+	echo "\$something is SET\n";
+}
+// Serialize the array $array, and output the results
+
+$serial = serialize($array);
+echo $serial . PHP_EOL;
+// Unserialize the array $array, and output the results
+$unserial = unserialize($serial);
+var_dump($unserial);
+
+
+?>
